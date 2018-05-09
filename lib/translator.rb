c120616dd3d99eaf9cc914202ca87d7a4a8cbca8
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-09 10:57:21 -0600

    Final commit

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Translator
 
   def initialize
@@ -49,21 +51,55 @@ class Translator
   end
 
   def morse_to_eng(string)
-    array_of_characters = string.split(" ")
-    array_of_characters.map! do |character|
+    array_of_characters_raw = string.split("")
+    array_with_spaces = []
+    array_of_characters_raw.each do |character|
+      if character == " "
+        array_with_spaces << character
+      else
+        array_with_spaces << character
+      end
+    end
+    array_with_spaces.map! do |character|
       character = @dictionary.key(character)
     end
-    english_string = array_of_characters.join("")
+    english = array_with_spaces.join("")
   end
 
-  def eng_to_morse_from_file
+  # def morse_to_eng(string)
+  #   array_raw = string.split("")
+  #   array_with_spaces = []
+  #   array_raw.each do |character|
+  #     while character != " "
+  #       array_with_spaces << character
+  #       array_with_spaces.concat
+  #       next
+  #     end
+  #     array_with_spaces.map! do |character|
+  #       word = @dictionary.key(character)
+  #     end
+  #   end
+  # end
+
+
+  def from_file(file)
+    file_lines_array = File.readlines(file)
+    file_characters_array = []
+    file_lines_array.each do |line|
+      file_characters_array << line
+    end
+    file_characters_array.map! do |character|
+      character = @dictionary[character]
+    end
+    morse_code = file_characters_array.join("")
+    puts morse_code
   end
 
 end
 
 t = Translator.new
 puts t.eng_to_morse("hello world")
-puts t.eng_to_morse("There are 3 ships")
+
+puts t.from_file('./input.txt')
 
 puts t.morse_to_eng(".... . .-.. .-.. ---  .-- --- .-. .-.. -..")
-# puts t.morse_to_eng("-......-.. .-.-.. ...-- ..........--....")
