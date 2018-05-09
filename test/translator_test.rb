9806f3e4e9397f844913aa2e01e58086ee1c1b40
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-09 10:46:41 -0600

    Update tests for 'from_file' and 'morse_to_eng'

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -9,7 +9,7 @@ class TranslatorTest < Minitest::Test
     assert_instance_of Translator, t
   end
 
-  def test_if_to_translates_eng_to_morse
+  def test_if_translates_eng_to_morse
     t = Translator.new
     letter = t.eng_to_morse("a")
     number = t.eng_to_morse("9")
@@ -21,7 +21,22 @@ class TranslatorTest < Minitest::Test
     assert_equal phrase, "-......-.. .-.-.. ...-- ..........--...."
   end
 
-  def test_if
+  def test_if_translates_from_file
+    t = Translator.new
+    file_line_in_morse = t.from_file('./input.txt')
+    assert_equal file_line_in_morse, ".. .--- ..-. .- ..-....-..."
+  end 
+
+  def test_if_translates_morse_to_eng
+    t = Translator.new
+    letter = t.eng_to_morse(".-")
+    number = t.eng_to_morse("----.")
+    greeting = t.eng_to_morse("......-...-..--- .-----.-..-..-..")
+    phrase = t.eng_to_morse("-......-.. .-.-.. ...-- ..........--....")
+    assert_equal letter, "a"
+    assert_equal number, "9"
+    assert_equal greeting, "hello world"
+    assert_equal phrase, "there are 3 ships"
   end
 
 
