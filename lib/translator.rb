dfb436a45f62b9e5e4e1aba98dc7fbebea9ea4d1
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-09 09:44:50 -0600

    Initial commit; add eng_to_morse

--- lib/translator.rb
+++ lib/translator.rb
@@ -39,4 +39,31 @@ class Translator
                     "0" => "-----",
                     " " => " "}
   end
+
+  def eng_to_morse(string)
+    array_of_characters = string.downcase.split("")
+    array_of_characters.map! do |character|
+      character = @dictionary[character]
+    end
+    morse_code = array_of_characters.join("")
+  end
+
+  def morse_to_eng(string)
+    array_of_characters = string.split(" ")
+    array_of_characters.map! do |character|
+      character = @dictionary.key(character)
+    end
+    english_string = array_of_characters.join("")
+  end
+
+  def eng_to_morse_from_file
+  end
+
 end
+
+t = Translator.new
+puts t.eng_to_morse("hello world")
+puts t.eng_to_morse("There are 3 ships")
+
+puts t.morse_to_eng(".... . .-.. .-.. ---  .-- --- .-. .-.. -..")
+# puts t.morse_to_eng("-......-.. .-.-.. ...-- ..........--....")
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,28 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator.rb'
+
+class TranslatorTest < Minitest::Test
+
+  def test_does_translator_exist
+    t = Translator.new
+    assert_instance_of Translator, t
+  end
+
+  def test_if_to_translates_eng_to_morse
+    t = Translator.new
+    letter = t.eng_to_morse("a")
+    number = t.eng_to_morse("9")
+    greeting = t.eng_to_morse("hello world")
+    phrase = t.eng_to_morse("there are 3 ships")
+    assert_equal letter, ".-"
+    assert_equal number, "----."
+    assert_equal greeting, "......-...-..--- .-----.-..-..-.."
+    assert_equal phrase, "-......-.. .-.-.. ...-- ..........--...."
+  end
+
+  def test_if
+  end
+
+
+end
